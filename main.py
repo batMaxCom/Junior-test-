525a15be03a5ac3ef149df5ef58a85f5437220de
batMaxCom <batman181194@gmail.com>
2022-12-05 16:14:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import re
+
+"""1"""
+def to_camel_case(text):
+    return re.split('_|-', text)[1] + ''.join(word.title() for word in re.split('-|_', text)[1:])
+
+"""2"""
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+"""3"""
+count_bits = lambda n: bin(n).count('1')
+
+"""4"""
+def digital_root(n):
+    return n if n < 10 else digital_root(sum(map(int,str(n))))
+
+"""5"""
+even_or_odd = lambda number: "Even" if number % 2 == 0 else "Odd"
